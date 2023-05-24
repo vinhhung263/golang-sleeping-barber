efefd63e50284233ea1f28458934efeff077839a
vinhhung263 <hungpv263@gmail.com>
2023-05-24 10:29:04 +0700

    init

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+var seatingCapacity = 10
+var arrivalRate = 100
+var cutDuration = 1 * time.Second
+var timeOpen = 10 * time.Second
+
+func main() {
+	// seed our random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	color.Yellow("The Sleeping Barber Problem")
+	color.Yellow("---------------------------")
+
+	clientChan := make(chan string, seatingCapacity)
+	doneChan := make(chan bool)
+
+	shop := BarberShop{
+		ShopCapacity:    seatingCapacity,
+		HairCutDuration: cutDuration,
+		NumberOfBarbers: 0,
+		ClientsChan:     clientChan,
+		BarbersDoneChan: doneChan,
+		Open:            true,
+	}
+
+	color.Green("The shop is open for the day!")
+
+	shop.addBarber("Frank")
+
+	shopClosing := make(chan bool)
+	closed := make(chan bool)
+
+	go func() {
+		<-time.After(timeOpen)
+		shopClosing <- true
+		shop.closeShopForDay()
+		closed <- true
+	}()
+
+	// add clients
+	i := 1
+	go func() {
+		for {
+			randomMillseconds := rand.Int() % (2 * arrivalRate)
+			select {
+			case <-shopClosing:
+				return
+			case <-time.After(time.Millisecond * time.Duration(randomMillseconds)):
+				shop.addClient(fmt.Sprintf("Client #%d", i))
+				i++
+			}
+		}
+	}()
+
+	// block until the barbershop is closed
+	<-closed
+}
